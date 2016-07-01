f019e6b1e33efc2822b2c430edd0ef70f1614689
PolAndrew <andrew_polukhtovych@yahoo.com>
2016-07-01 11:32:48 +0300

    Test Grid

--- src/test/java/org/spreadshit/FactoryTest.java
+++ src/test/java/org/spreadshit/FactoryTest.java
@@ -2,6 +2,9 @@ package org.spreadshit;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class FactoryTest {
@@ -15,4 +18,17 @@ public class FactoryTest {
 		assertTrue("error cell created", factory.createCell("124BB") instanceof ErrorCell);
 		assertTrue("error cell created", factory.createCell("=A1--B2w") instanceof ErrorCell);
 	}
+	
+	public void testcreatGrid(){
+		Factory factory=new Factory();
+		List<String> cells = new ArrayList();
+		cells.add("'ABC\t22\t35");
+		cells.add("12\t333\t800");
+		Grid grid = factory.createGrid(cells);
+		assertTrue("Expect cells created ok", grid.getCellAt(2, B) instanceof NumberCell);
+		
+		
+		
+	}
+	
 }
