e9e13e44998a74691fbb236d05cadd318976a55d
PolAndrew <andrew_polukhtovych@yahoo.com>
2016-07-05 09:37:11 +0300

    Correct Grid

--- src/main/java/org/spreadshit/Factory.java
+++ src/main/java/org/spreadshit/Factory.java
@@ -7,11 +7,13 @@ import java.util.regex.Pattern;
 
 public class Factory {
 	private static final Pattern pattern = Pattern.compile("\t");
-	private static final Pattern expressPat = Pattern.compile("^=([a-zA-Z][1-9]|[0-9]+)([+*/-]([a-zA-Z][0-9]|[0-9]+))*$");
+	private static final Pattern expressPat = Pattern
+			.compile("^=([a-zA-Z][1-9]|[0-9]+)([+*/-]([a-zA-Z][0-9]|[0-9]+))*$");
 	private static final Pattern labelPat = Pattern.compile("^'");
 	private static final Pattern numberPat = Pattern.compile("\\d");
+	Grid grid = new Grid();
 	String[] dataFromScan = pattern.split(Scan.scStr());
-	List<String> line=Arrays.asList(dataFromScan);
+	List<String> line = Arrays.asList(dataFromScan);
 
 	public Cell createCell(String s) {
 		if (expressPat.matcher(s) != null) {
@@ -28,17 +30,15 @@ public class Factory {
 
 	}
 
-	
-
 	public Grid CreateGrid(List<String> line) {
-		Grid grid=new Grid();
-		ArrayList<Cell> CellLine=new ArrayList<Cell>();
-				
+
+		ArrayList<Cell> CellLine = new ArrayList<Cell>();
+
 		for (String cell : line) {
 			CellLine.add(createCell(cell));
 		}
 		grid.addToTable(CellLine);
-		
+
 		return grid;
 	}
 
